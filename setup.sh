6719144535ddbb04b64e6670d9b268836c6fa404
cm1320 <doyun.shin@gmail.com>
2019-07-24 12:27:18 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install git ffmpeg python3.5 python3-pip
+python3.5 -m pip install --upgrade pip
+python3.5 -m pip install --upgrade -r requirements.txt
+
+echo now you can run "python3.5 app.py"
