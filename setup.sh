3d2c1f0c11444eb3ad6cb7488138014b35e3a9c6
Roul_ <34963865+SDR-Roul@users.noreply.github.com>
2019-08-01 20:04:51 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install git ffmpeg python3.5 python3-pip
+sudo apt install git ffmpeg python3.5 python3-pip -y
 python3.5 -m pip install --upgrade pip
 python3.5 -m pip install --upgrade -r requirements.txt
 
